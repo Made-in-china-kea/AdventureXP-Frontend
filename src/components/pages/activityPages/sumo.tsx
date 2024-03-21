125829c150ee530101b4c5bdd997dd4348280a0b
Sander Johansen <55443844+SanderJohansen@users.noreply.github.com>
2024-03-21 12:19:21 +0100

    all activity pages are up

---
+++ src/components/pages/activityPages/sumo.tsx
@@ -0,0 +1,27 @@
+
+import backgroundImage from '../../../assets/Images/backgrounds/sumoBackground.png'; 
+import {Button} from '../../Button.tsx'
+
+const Minigolf = () => {
+  return (
+    <div className="hero min-h-screen bg-no-repeat bg-cover bg-center"  style={{backgroundImage: `url(${backgroundImage})`}}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <Button
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          type="button"
+          onClick={() => {}}
+        >
+          Book Here!
+        </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Minigolf;
